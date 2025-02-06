bd89c661ed392ecb4c2721482182b687be20deb6
docternath <doctermath@gmail.com>
2025-02-06 17:09:32 +0700

    add gen empnam alias

---
+++ python/empnam_alias_gen/genEmpnamAlias.py
@@ -0,0 +1,8 @@
+def generate_alias(name: str) -> str:
+    if len(name) <= 20:
+        return name
+    return "".join(word[0].upper() for word in name.split())
+
+# Example usage
+print(generate_alias("Jonathan Christopher Smith"))  # Output: JCS
+print(generate_alias("Short Name"))
